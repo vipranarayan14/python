ab378777d5b36c76e93f748546019fad46aec642
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-08-16 22:23:03 +0530

    using  and  file

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,11 @@ print("Hello World!")
 age = 20
 name = "Rama"
 
-print("{0}'s age is {1}".format(name,age))
+print("{0}\'s age is {1}".format(name,age)) + "\nAnd he said 'Hello'"
+
+print("{0:-^100}".format("hello"))
+
+print("{0:.10f}".format(1.0/3))
+
+f = open('open.txt', 'w')
+f.write(name)
